d26d63ccadc8b49e37203c742576877bb061f14e
takayuki <takayuki@idein.jp>
2018-03-14 09:33:22 +0000

    cleanup: delete unneeded options
    remove: pkgconfig (unused)

--- build.rs
+++ build.rs
@@ -1,30 +1,10 @@
 extern crate bindgen;
-extern crate pkg_config;
 
 use std::env;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    // Tell cargo to tell rustc to link the system rpigrafx shared library.
-    let rpigrafx = pkg_config::probe_library("librpigrafx").unwrap();
-
-    let rpigrafx_args: Vec<_> = rpigrafx
-        .include_paths
-        .iter()
-        .flat_map(|path| vec!["-I", path.to_str().unwrap()])
-        .collect();
-
-    // Path to directories of C header
-    let include_dirs: Vec<PathBuf> = vec![
-        Path::new(&env::var("C_INCLUDE_PATH")
-            .expect("C_INCLUDE_PATH like: /usr/lib/llvm-3.9/lib/clang/3.9.1/include"))
-            .into(),
-    ];
-
-    let include_args: Vec<_> = include_dirs
-        .iter()
-        .flat_map(|path| vec!["-I", path.to_str().unwrap()])
-        .collect();
+    println!("cargo:rustc-link-lib=qmkl");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -33,8 +13,6 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
-        .clang_args(&rpigrafx_args)
-        .clang_args(&include_args)
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
